22c49bd3159f997b01e7e8859ae86afab4bac119
romanov <romanovnospam@outlook.com>
2016-04-11 13:31:23 +0300

    MonoGame 3.5.1 support

--- MonoGameConsole/Properties/AssemblyInfo.cs
+++ MonoGameConsole/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MonoGameConsole")]
+[assembly: AssemblyTitle("MonoGame-Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MonoGameConsole")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("MonoGame-Console")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
